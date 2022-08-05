6b5f4af926cb012974569ac377a434bd54913438
arturfedotiev <artur.fedotiew@gmail.com>
2022-08-05 06:53:01 +0300

    first commit

---
+++ apps/omnia-retail/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'omnia-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'omnia-retail';
+}
