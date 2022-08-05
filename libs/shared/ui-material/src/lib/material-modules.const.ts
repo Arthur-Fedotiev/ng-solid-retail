0f7fee8dba8085a19291b63a38377658dd3458b2
arturfedotiev <artur.fedotiew@gmail.com>
2022-08-05 10:05:03 +0300

    feat/1: created shared material module

---
+++ libs/shared/ui-material/src/lib/material-modules.const.ts
@@ -0,0 +1,61 @@
+import { MatTreeModule } from '@angular/material/tree';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatListModule } from '@angular/material/list';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+export const MATERIAL_MODULES = [
+  MatButtonModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatCardModule,
+  MatRadioModule,
+  MatTreeModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatProgressBarModule,
+  MatExpansionModule,
+  MatTableModule,
+  MatBadgeModule,
+  MatIconModule,
+  MatGridListModule,
+  MatListModule,
+  MatSortModule,
+  MatTooltipModule,
+  MatSliderModule,
+  MatDialogModule,
+  MatChipsModule,
+  MatAutocompleteModule,
+  MatStepperModule,
+  MatCheckboxModule,
+  MatMenuModule,
+  MatTabsModule,
+  MatProgressSpinnerModule,
+];
