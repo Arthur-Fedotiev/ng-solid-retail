aebc18e928b183c948d35c23426e85c6c0c06b53
arturfedotiev <artur.fedotiew@gmail.com>
2022-08-13 07:48:49 +0300

    feat/10: added uni tests for VM mappers

---
+++ libs/products/data-access/src/lib/products-data-access.module.spec.ts
@@ -0,0 +1,7 @@
+import { ProductsDataAccessModule } from './products-data-access.module';
+
+describe('ProductsDataAccessModule', () => {
+  it('should create an instance', () => {
+    expect(new ProductsDataAccessModule()).toBeTruthy();
+  });
+});
---
+++ libs/products/data-access/src/lib/utils/to-products-by-price.spec.ts
@@ -0,0 +1,11 @@
+import { makeProductViewModelsStub } from '../testing/make-product-view-models-stub';
+import { toProductsByPrice } from './to-products-by-price';
+
+describe('#toProductsByPrice', () => {
+  it('should return a ProductByPrice', () => {
+    const productsStub = makeProductViewModelsStub(2);
+    const result = productsStub.reduce(toProductsByPrice, []);
+
+    expect(result).toMatchSnapshot();
+  });
+});
