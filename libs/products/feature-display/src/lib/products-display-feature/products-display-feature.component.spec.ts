48fb9a33ec0dff4bb8ec7d3c2dd6035a51ca77e8
arturfedotiev <artur.fedotiew@gmail.com>
2023-02-18 10:28:25 +0100

    test: integration test ProductsDisplayFeatureComponent

--- libs/products/feature-display/src/lib/products-display-feature/products-display-feature.component.spec.ts
+++ libs/products/feature-display/src/lib/products-display-feature/products-display-feature.component.spec.ts
@@ -1,58 +1,96 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ProductsFacadeService } from '@sr/products/application';
-
-import { Subject } from 'rxjs';
+import { TestBed } from '@angular/core/testing';
+import { SELECT_PRODUCT_COMMAND } from '@sr/products/application';
+import { render } from '@testing-library/angular';
+import { BehaviorSubject } from 'rxjs';
 
 import { ProductsDisplayFeatureComponent } from './products-display-feature.component';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { ProductCardComponent } from '@sr/products/ui';
+import { RESOLVED_VM } from '@sr/shared/util';
+import { DebugElement } from '@angular/core';
 
 describe('ProductsDisplayFeatureComponent', () => {
-  const productsSelectorStub$ = new Subject();
+  describe('render', () => {
+    it('should render products', async () => {
+      const { renderResult, productsShortInfo } = await setup();
 
-  let component: ProductsDisplayFeatureComponent;
-  let fixture: ComponentFixture<ProductsDisplayFeatureComponent>;
-  let facadeService: jest.Mocked<ProductsFacadeService>;
+      const productCards = getProductCards(renderResult.debugElement);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ProductsDisplayFeatureComponent],
-      providers: [
-        {
-          provide: ProductsFacadeService,
-          useValue: {
-            productsShortInfo$: productsSelectorStub$,
-            productSelected: jest.fn(),
-          },
-        },
-        {
-          provide: Router,
-          useValue: { getCurrentNavigation: jest.fn() },
-        },
-      ],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(ProductsDisplayFeatureComponent);
-    facadeService = TestBed.inject(
-      ProductsFacadeService
-    ) as jest.Mocked<ProductsFacadeService>;
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      expect(productCards.length).toEqual(productsShortInfo.length);
+    });
   });
+  describe('when product selected', () => {
+    it('should delegate select product on click', async () => {
+      const { renderResult, selectProductCommandSpy, productsShortInfo } =
+        await setup();
+      const productId = productsShortInfo[0].id;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+      getFirstProductCard(renderResult.debugElement).triggerEventHandler(
+        'click',
+        { productId }
+      );
 
-  describe('#onClick', () => {
-    it('should delegate select product on click', () => {
-      const productId = 'product-id';
-
-      component.onClick(productId);
-
-      expect(facadeService.productSelected).toHaveBeenNthCalledWith(
+      expect(selectProductCommandSpy.execute).toHaveBeenNthCalledWith(
         1,
         productId
       );
     });
   });
+
+  async function setup({ productsShortInfo = createProductsStub() } = {}) {
+    const productsSelectorStub$ = new BehaviorSubject({ productsShortInfo });
+
+    const renderResult = await render(ProductsDisplayFeatureComponent, {
+      imports: [ProductsDisplayFeatureComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                vm$: productsSelectorStub$,
+              },
+            },
+          },
+        },
+        {
+          provide: SELECT_PRODUCT_COMMAND,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+      ],
+    });
+
+    return {
+      renderResult,
+      productsShortInfo,
+      selectProductCommandSpy: TestBed.inject(SELECT_PRODUCT_COMMAND),
+      vm$: TestBed.inject(ActivatedRoute).snapshot.data[RESOLVED_VM],
+    };
+  }
+
+  function createProductsStub() {
+    return Array.from({ length: 3 }, (_, idx) => createProductStub(idx));
+  }
+
+  function createProductStub(idx: number) {
+    return {
+      id: `product-id-${idx}`,
+      name: `product-name-${idx}`,
+      sku: `product-sku-${idx}`,
+      url: `product-url-${idx}`,
+      price: idx,
+      retailer: `product-retailer-${idx}`,
+    };
+  }
+
+  function getFirstProductCard(debugEl: DebugElement) {
+    return debugEl.query(By.directive(ProductCardComponent));
+  }
+
+  function getProductCards(debugEl: DebugElement) {
+    return debugEl.queryAll(By.directive(ProductCardComponent));
+  }
 });
