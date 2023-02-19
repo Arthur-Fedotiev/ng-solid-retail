17fd0b28c0e8ec5b8e6e8b998c9f107702dc8397
arturfedotiev <artur.fedotiew@gmail.com>
2023-02-19 06:41:27 +0100

    test: unit test ProductsNavigationManagerService

--- libs/products/application/src/lib/navigation/products-navigation-manager.service.spec.ts
+++ libs/products/application/src/lib/navigation/products-navigation-manager.service.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ProductsNavigationManagerService } from './products-navigation-manager.service';
+import { Router } from '@angular/router';
+import { ROUTES } from './constants';
 
 describe('ProductsNavigationManagerService', () => {
-  let service: ProductsNavigationManagerService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductsNavigationManagerService);
+  it('should navigate to display', () => {
+    const { service, navigateByUrlSpy } = setup();
+    service.navigateToDisplay();
+    expect(navigateByUrlSpy).toHaveBeenCalledWith(ROUTES.display());
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should navigate to product', () => {
+    const { service, navigateByUrlSpy } = setup();
+    const id = '1';
+    service.navigateToProduct(id);
+    expect(navigateByUrlSpy).toHaveBeenCalledWith(ROUTES.details(id));
   });
+
+  function setup() {
+    const routerMock = {
+      navigateByUrl: jest.fn(),
+    } as unknown as Router;
+
+    const service = new ProductsNavigationManagerService(routerMock);
+    return { service, navigateByUrlSpy: routerMock.navigateByUrl };
+  }
 });
