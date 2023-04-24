eb04377f1e3e32c8d02c3122cd33ea90c4c54f25
arturfedotiev <artur.fedotiew@gmail.com>
2023-04-24 07:57:44 +0200

    generate: generate open api types

---
+++ libs/generated/solid-retail-api-types/src/models/index.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ApiV1CataloguePost201Response';
+export * from './ApiV1CataloguePostRequest';
+export * from './BookSpecification';
+export * from './Category';
+export * from './ClothingSpecification';
+export * from './CreateBookRequest';
+export * from './CreateBookResponse';
+export * from './CreateClothingRequest';
+export * from './CreateClothingResponse';
+export * from './CreateProductRequest';
+export * from './CreateProductResponse';
+export * from './CreateShoesRequest';
+export * from './CreateShoesResponse';
+export * from './Price';
+export * from './PriceResponse';
+export * from './ShoesSpecification';
