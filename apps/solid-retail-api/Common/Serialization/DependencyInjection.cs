e34e8e99ff7f090221e64618a7a270b3d883ef65
arturfedotiev <artur.fedotiew@gmail.com>
2023-06-15 07:46:11 +0200

    refactor: move converters configurations to dedicated methods

--- apps/solid-retail-api/Common/Serialization/DependencyInjection.cs
+++ apps/solid-retail-api/Common/Serialization/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json.Serialization;
 using JsonSubTypes;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Converters;
 using Sr.Api.ProductsCatalogue.Common;
 using Sr.Api.ProductsCatalogue.Contracts.CreateProduct;
@@ -12,32 +13,45 @@ namespace Sr.SolidRetailApi.Common.Serialization
     public static IServiceCollection AddControllersConfiguration(this IServiceCollection services)
     {
       _ = services.AddControllers()
-          .AddNewtonsoftJson(options =>
-          {
-            options.SerializerSettings.Converters.Add(new StringEnumConverter());
-            options
-              .SerializerSettings.Converters
-                .Add(JsonSubtypesConverterBuilder.Of(typeof(CreateProductRequest), nameof(CreateProductRequest.Category))
-                  .RegisterSubtype(typeof(CreateBookRequest), ProductCategory.Books)
-                  .RegisterSubtype(typeof(CreateShoesRequest), ProductCategory.Shoes)
-                  .RegisterSubtype(typeof(CreateClothingRequest), ProductCategory.Clothing)
-                  .SerializeDiscriminatorProperty()
-                  .Build());
-            options.SerializerSettings.Converters
-                .Add(JsonSubtypesConverterBuilder.Of(typeof(UpdateProductRequest), nameof(UpdateProductRequest.Category))
-                  .RegisterSubtype(typeof(UpdateBookRequest), ProductCategory.Books)
-                  .RegisterSubtype(typeof(UpdateShoesRequest), ProductCategory.Shoes)
-                  .RegisterSubtype(typeof(UpdateClothingRequest), ProductCategory.Clothing)
-                  .SerializeDiscriminatorProperty()
-                  .Build());
-          })
-          .AddJsonOptions(options =>
-          {
-            options.JsonSerializerOptions.PropertyNamingPolicy = null;
-            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-          });
+          .AddNewtonsoftJson(SetupNewtonsoftJson)
+          .AddJsonOptions(SetupJsonOptions);
 
       return services;
+    }
+
+    private static void SetupNewtonsoftJson(MvcNewtonsoftJsonOptions options)
+    {
+      options.SerializerSettings.Converters.Add(new StringEnumConverter());
+      AddCreateProductRequestConverter(options);
+      AddUpdateProductRequestConverter(options);
+    }
+
+    private static void SetupJsonOptions(JsonOptions options)
+    {
+      options.JsonSerializerOptions.PropertyNamingPolicy = null;
+      options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    }
+
+    private static void AddCreateProductRequestConverter(MvcNewtonsoftJsonOptions options)
+    {
+      options.SerializerSettings.Converters
+        .Add(JsonSubtypesConverterBuilder.Of(typeof(CreateProductRequest), nameof(CreateProductRequest.Category))
+          .RegisterSubtype(typeof(CreateBookRequest), ProductCategory.Books)
+          .RegisterSubtype(typeof(CreateShoesRequest), ProductCategory.Shoes)
+          .RegisterSubtype(typeof(CreateClothingRequest), ProductCategory.Clothing)
+          .SerializeDiscriminatorProperty()
+          .Build());
+    }
+
+    private static void AddUpdateProductRequestConverter(MvcNewtonsoftJsonOptions options)
+    {
+      options.SerializerSettings.Converters
+        .Add(JsonSubtypesConverterBuilder.Of(typeof(UpdateProductRequest), nameof(UpdateProductRequest.Category))
+          .RegisterSubtype(typeof(UpdateBookRequest), ProductCategory.Books)
+          .RegisterSubtype(typeof(UpdateShoesRequest), ProductCategory.Shoes)
+          .RegisterSubtype(typeof(UpdateClothingRequest), ProductCategory.Clothing)
+          .SerializeDiscriminatorProperty()
+          .Build());
     }
   }
 }
