5bb5499b2cad3b0ff48acfcf10f4d17a6bc295c3
arturfedotiev <artur.fedotiew@gmail.com>
2023-08-06 11:09:58 +0200

    build: lint --fix

--- libs/products/feature-details/src/lib/product-details/lowest-tier-price.pipe.ts
+++ libs/products/feature-details/src/lib/product-details/lowest-tier-price.pipe.ts
@@ -7,7 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LowestTierPricePipe implements PipeTransform {
   transform(
     value: readonly { price: number; tier: number }[],
-    tier: number = 1
+    tier = 1
   ): number | null {
     const lowestPrice = value.reduce((prev: number | null, curr) => {
       return curr.tier === tier && prev ? Math.min(prev, curr.price) : prev;
