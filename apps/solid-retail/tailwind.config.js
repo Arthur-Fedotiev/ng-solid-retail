56d33500ea30d7a06e0465bcb9ab3ae225f989ca
arturfedotiev <artur.fedotiew@gmail.com>
2023-08-22 12:01:06 +0200

    build: update tailwind config

--- apps/solid-retail/tailwind.config.js
+++ apps/solid-retail/tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
+    'apps/**/*.{html, ts}',
+    'libs/**/*.{html, ts}',
   ],
   theme: {
     extend: {},
